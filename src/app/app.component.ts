529fd07dce9acdf351ab988a5f91972735d35e69
drmrhdt <drmrhdt@gmail.com>
2021-12-15 00:17:02 +0300

    feat: add an elevator's panel with 10 buttons, and save last selected button value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'elevator';
+  floors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  activeFloor = 1;
+
+  onClickFloorBtn(i: number) {
+    this.activeFloor = i;
+  }
 }
